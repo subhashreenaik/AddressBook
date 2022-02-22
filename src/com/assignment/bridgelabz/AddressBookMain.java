97433ca560558193a7d615f8f3fa46b8d198e2ab
Subhashree naik <subhashree.naik@gmail.com>
2022-02-22 21:55:14 +0530

    Added:Welcome message

---
+++ src/com/assignment/bridgelabz/AddressBookMain.java
@@ -0,0 +1,10 @@
+package com.assignment.bridgelabz;
+
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("Welcome to Address Book Problem");     
+	}
+
+}
