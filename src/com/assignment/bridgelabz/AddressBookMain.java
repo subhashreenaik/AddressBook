1710196e10e5f32e3166fee4d5a7beb9ef09305b
Subhashree naik <subhashree.naik@gmail.com>
2022-02-24 17:07:24 +0530

    Added:Editing existing contact

--- src/com/assignment/bridgelabz/AddressBookMain.java
+++ src/com/assignment/bridgelabz/AddressBookMain.java
@@ -5,83 +5,98 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class AddressBookMain {
 	
-	//Declaring variable
-	static String fname,lname,add,city,state,zip,phoneno,emailid;
+    //Declaring variable
+	static String fname, lname, add, city, state, zip, phoneno, emailid;
 	private String addressId;
-    List<PersonInfo> infos;
-    static List<PersonInfo> contact;
-    
-    
-   
-   //constructor 
-    public AddressBookMain(){
-    	 contact = new ArrayList<PersonInfo>();
-        }
-  
-    public String getaddressId() {
-        return addressId;
-    }
-  
-    public void setaddressId(String addressId) {
-        this.addressId = addressId;
-    }
-    public List<PersonInfo> getPersonInfo() {
-        return infos;
-    }
-    public  void setPersonInfo(List<PersonInfo> infos) {
-        this.infos = infos;
-    }
-    
-	//Adding information of a person
-	  public PersonInfo enterInform() {
-		Scanner sc =new Scanner(System.in);
-		System.out.println("Enter your First name :");
-	    fname = sc.nextLine();
-		System.out.println("Enter your Last name : ");
-	    lname = sc.nextLine();
-		System.out.println("Enter your address :");
-	    add = sc.nextLine();
-		System.out.println("Enter your city :");
-	    city = sc.nextLine();
-		System.out.println("Enter your state :");
-	    state = sc.nextLine();
-		System.out.println("Enter your zip :");
-	    zip= sc.nextLine();
-		System.out.println("Enter your phoneno :");
-	    phoneno = sc.nextLine();
-		System.out.println("Enter your email id :");
-	    emailid = sc.nextLine();
-	    
-	    PersonInfo person = new PersonInfo(fname,lname,add,city,state,zip,phoneno,emailid);
-		contact.add(person);
-		return person;
-		
-	
-	}
-	
-	
-	
-	
-	
+	List<PersonInfo> infos;
+	static List<PersonInfo> contact;
 
+
+	public AddressBookMain() {
+		contact = new ArrayList<PersonInfo>();
+	}
+
+	public String getaddressId() {
+		return addressId;
+	}
+
+	public void setaddressId(String addressId) {
+		this.addressId = addressId;
+	}
+
+	public List<PersonInfo> getPersonInfo() {
+		return infos;
+	}
+
+	public void setPersonInfo(List<PersonInfo> infos) {
+		this.infos = infos;
+	}
+
+	public void enterInform() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your First name :");
+		fname = sc.nextLine();
+		System.out.println("Enter your Last name : ");
+		lname = sc.nextLine();
+		System.out.println("Enter your address :");
+		add = sc.nextLine();
+		System.out.println("Enter your city :");
+		city = sc.nextLine();
+		System.out.println("Enter your state :");
+		state = sc.nextLine();
+		System.out.println("Enter your zip :");
+		zip = sc.nextLine();
+		System.out.println("Enter your phoneno :");
+		phoneno = sc.nextLine();
+		System.out.println("Enter your email id :");
+		emailid = sc.nextLine();
+
+
+	}
+   
+	 public static void callMethod() {
+		 PersonInfo person = new PersonInfo(fname, lname, add, city, state, zip, phoneno, emailid);
+		 contact.add(person);
+	 }
+	 
+	
 	public static void main(String[] args) {
-		
-		AddressBookMain  address = new AddressBookMain();
-		
-		//for person1
+		AddressBookMain address = new AddressBookMain();
 		address.enterInform();
+		callMethod();
 		address.setPersonInfo(contact);
 		
-		//for person 2
-		address.enterInform();
-		address.setPersonInfo(contact);
+		for (PersonInfo gen : contact) {
+			System.out.println(gen);
+		}
+
+		// Edit the information with name
+		System.out.println("Enter the name of the person you want to add");
+		Scanner s = new Scanner(System.in);
+		String name = s.nextLine();
+
+		for (int j = 0; j < contact.size(); j++) {
+			PersonInfo p = contact.get(j);
+			if (name.equals(p.getFirstName())) {
+				address.enterInform();
+				PersonInfo person3 = new PersonInfo(fname, lname, add, city, state, zip, phoneno, emailid);
+				contact.set(j, person3);
+
+			}
+		}
 		
-		for(PersonInfo gen : address.getPersonInfo())
-		{
-		    System.out.println(gen);
-		}		
+
+		for (PersonInfo gen1 : contact) {
+			System.out.println("Updated information is "+gen1);
+		}
+
 	}
 
 }
