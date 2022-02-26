ab26ff2b2a5cfbd210d4918e7e748d43acd83b4a
Subhashree naik <subhashree.naik@gmail.com>
2022-02-26 16:54:18 +0530

    Added:multiple address book through hashmap

--- src/com/assignment/bridgelabz/AddressBookMain.java
+++ src/com/assignment/bridgelabz/AddressBookMain.java
@@ -2,7 +2,9 @@ package com.assignment.bridgelabz;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 import java.util.ArrayList;
@@ -14,21 +16,27 @@ public class AddressBookMain {
 	
     //Declaring variable
 	static String fname, lname, add, city, state, zip, phoneno, emailid;
-	private String addressId;
+	private static String addressId;
 	List<PersonInfo> infos;
 	static List<PersonInfo> contact;
+	static Map<String, List<PersonInfo>> addressBook;
 
+
+	public Map<String, List<PersonInfo>> getAddressBook() {
+		return addressBook;
+	}
+
+	public void setAddressBook(Map<String, List<PersonInfo>> addressBook) {
+		this.addressBook = addressBook;
+	}
 
 	public AddressBookMain() {
 		contact = new ArrayList<PersonInfo>();
+		addressBook = new HashMap<>();
 	}
-
+	
 	public String getaddressId() {
 		return addressId;
-	}
-
-	public void setaddressId(String addressId) {
-		this.addressId = addressId;
 	}
 
 	public List<PersonInfo> getPersonInfo() {
@@ -39,8 +47,8 @@ public class AddressBookMain {
 		this.infos = infos;
 	}
 
-	public void enterInform() {
-		Scanner sc = new Scanner(System.in);
+	public static  void enterInform() {
+	     Scanner sc = new Scanner(System.in);
 		System.out.println("Enter your First name :");
 		fname = sc.nextLine();
 		System.out.println("Enter your Last name : ");
@@ -57,13 +65,14 @@ public class AddressBookMain {
 		phoneno = sc.nextLine();
 		System.out.println("Enter your email id :");
 		emailid = sc.nextLine();
-
+		
 
 	}
    
 	 public static void callMethod() {
 		 PersonInfo person = new PersonInfo(fname, lname, add, city, state, zip, phoneno, emailid);
 		 contact.add(person);
+		 addressBook.put(addressId, contact);
 	 }
 	 public void deletePerson() {
 		 System.out.println("Enter the name of the person you want to delete");
@@ -86,52 +95,79 @@ public class AddressBookMain {
 			}
 	 }
 	 
+	 public  void editPerson() {
+		
+		 System.out.println("Enter the name of the person you want to edit");
+			Scanner s = new Scanner(System.in);
+			String name = s.nextLine();
+			for (int j = 0; j < contact.size(); j++) {
+				PersonInfo p = contact.get(j);
+				if (name.equals(p.getFirstName())) {
+					enterInform();
+					PersonInfo person3 = new PersonInfo(fname, lname, add, city, state, zip, phoneno, emailid);
+					contact.set(j, person3);
+
+					for (PersonInfo gen1 : contact) {
+						System.out.println("Updated information is "+gen1);
+					}
+
+				}
+				else {
+					System.out.println("The name is different");
+				}
+			}
+//		
+	 }
+	 
 	
 	
 	public static void main(String[] args) {
 		AddressBookMain address = new AddressBookMain();
+		Scanner s = new Scanner(System.in);
 		
-		//Enter for person1
-		address.enterInform();
-		callMethod();
-		address.setPersonInfo(contact);
+		System.out.println("How many person's information You want to add");	
+		int n=s.nextInt();
+		System.out.println("Enter your address id :  address1 or address2");
+		s.nextLine();
+		addressId = s.nextLine();
+		switch(addressId) {
 		
-		//enter for person2
-		address.enterInform();
-		callMethod();
-		address.setPersonInfo(contact);
+		case "address1":
+			
+		for(int i=0;i<n;i++) {
+			enterInform();
+			callMethod();
+			address.setPersonInfo(contact);
+			address.setAddressBook(addressBook);
+		}
+		break;
+		case "address2":
+			for(int i=0;i<n;i++) {
+				enterInform();
+				callMethod();
+				address.setPersonInfo(contact);
+				address.setAddressBook(addressBook);
 		
+		}
+			break;
+		}
 		
+			
 		//printing the multiple person information
 		for (PersonInfo gen : contact) {
 			System.out.println(gen);
 		}
 		
+		//printing addressbook
+		System.out.println(address.getAddressBook());
 		
-
 		
-	   //Edit person information
-		System.out.println("Enter the name of the person you want to add");
-		Scanner s = new Scanner(System.in);
-		String name = s.nextLine();
-
-		for (int j = 0; j < contact.size(); j++) {
-			PersonInfo p = contact.get(j);
-			if (name.equals(p.getFirstName())) {
-				address.enterInform();
-				PersonInfo person3 = new PersonInfo(fname, lname, add, city, state, zip, phoneno, emailid);
-				contact.set(j, person3);
-
-			}
-			else {
-				System.out.println("The name is different");
-			}
-		}
-		for (PersonInfo gen1 : contact) {
-			System.out.println("Updated information is "+gen1);
-		}
-		 //Delete the information with name
-			address.deletePerson();
+	    //Edit person information
+		address.editPerson();
+		
+		
+		//Delete the information with name
+		address.deletePerson();
 		
 	}
 
